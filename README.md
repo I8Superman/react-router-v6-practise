51fd5bd2b7ed18c8f6ddf9794c0865fdb02502dd
I8Superman <simo414u@stud.kea.dk>
2022-04-21 14:55:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-bdu3fq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-bdu3fq)
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bookkeeper!</h1>
+    </div>
+  );
+}
